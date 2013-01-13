b8ad1bb1cf50bb6ff633abd91e226d898e469cbd
magicvoid <magicvoid@me.com>
2013-01-13 18:11:31 +0800

    222
    222

---
+++ src/com/funs/address/model/AddressVO.java
@@ -0,0 +1,79 @@
+/******************************************************************************
+ * Copyright (C) 2012 ShenZhen 1000funs Information Technology Co.,Ltd
+ * All Rights Reserved.
+ *****************************************************************************/
+package com.funs.address.model;
+
+/**
+ * AddressVO与RegionVO的区别是，Address包含了详细地址，而Region只是区域，如：香丽大厦、中投大厦。
+ * 
+ * @author Xingling
+ * @since JDK1.6
+ * @history 2013-01-13 Xingling build
+ */
+public class AddressVO {
+	private int id;
+	private String shortName;
+	private String fullName;
+	private String image;
+	private String userIp;
+	private int userId;
+	private String inputTime;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getUserIp() {
+		return userIp;
+	}
+
+	public void setUserIp(String userIp) {
+		this.userIp = userIp;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getInputTime() {
+		return inputTime;
+	}
+
+	public void setInputTime(String inputTime) {
+		this.inputTime = inputTime;
+	}
+
+}
