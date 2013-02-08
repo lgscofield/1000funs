1fb09b6c8a551ddb49dde92d47990098ea074a6e
jcchen <junchangchen@gmail.com>
2013-02-08 15:01:15 +0800

    完成用户中心原型html
    1. 增加用户中心原型html;
    2. 增加initData.sql,用于测试数据准备;

---
+++ initData.sql
@@ -0,0 +1,6 @@
+INSERT INTO `1000funs`.`package_group` (`id`, `group_name`, `image`, `detail`, `deleted`) VALUES ('1', '25元区', '/web/img/taochan2.jpg', '25元区;高级货', false);
+
+INSERT INTO `1000funs`.`package_group` (`id`, `group_name`, `image`, `detail`, `deleted`) VALUES ('2', '20元区', '/img/taochan1.jpg', '20元区;', false);
+
+INSERT INTO `1000funs`.`package_group` (`id`, `group_name`, `image`, `detail`, `deleted`) VALUES ('3', '18元区', '/web/img/taochan3.jpg', '18元区;', false);
+
