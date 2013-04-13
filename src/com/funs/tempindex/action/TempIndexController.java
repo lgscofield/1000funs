3d395f67e5023310511685c6c65c25ff3e0df319
magicvoid <magicvoid@me.com>
2013-04-13 11:01:52 +0800

    修复了上一次提交存在的不完善问题
    1.页面加载完后菜单才会出现
    2. fixed中文乱码问题

--- src/com/funs/tempindex/action/TempIndexController.java
+++ src/com/funs/tempindex/action/TempIndexController.java
@@ -1,12 +1,12 @@
 package com.funs.tempindex.action;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
 
 /**
@@ -24,17 +24,31 @@ function			method				url
 @RequestMapping("/tempindex")
 public class TempIndexController {
 	
+	public static Map<String,String> info = new HashMap<String,String>(15);
+	
+	/**
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping("/setmenu")
-	public String setMenu(HttpServletRequest request) {
-		ServletContext application = request.getSession().getServletContext();
-		application.setAttribute("price1", request.getAttribute("price1"));
-		application.setAttribute("price2", request.getAttribute("price2"));
-		//TODO ...
-		application.setAttribute("mainfood1", request.getAttribute("mainfood1"));
-		//TODO ...
-		application.setAttribute("secondfood1", request.getAttribute("secondfood1"));
-		//TODO ...
-		return "/web/tempindex.jsp";
+	@ResponseBody
+	public String setMenu(WebRequest request) {
+		info.put("price1", (String) request.getParameter("price1"));
+		info.put("price2", (String) request.getParameter("price2"));
+		info.put("price3", (String) request.getParameter("price3"));
+		info.put("price4", (String) request.getParameter("price4"));
+		info.put("price5", (String) request.getParameter("price5"));
+		info.put("mainfood1", (String) request.getParameter("mainfood1"));
+		info.put("mainfood2", (String) request.getParameter("mainfood2"));
+		info.put("mainfood3", (String) request.getParameter("mainfood3"));
+		info.put("mainfood4", (String) request.getParameter("mainfood4"));
+		info.put("mainfood5", (String) request.getParameter("mainfood5"));
+		info.put("secondfood1", (String) request.getParameter("secondfood1"));
+		info.put("secondfood2", (String) request.getParameter("secondfood2"));
+		info.put("secondfood3", (String) request.getParameter("secondfood3"));
+		info.put("secondfood4", (String) request.getParameter("secondfood4"));
+		info.put("secondfood5", (String) request.getParameter("secondfood5"));
+		return "update success";
 	}
 	
 }
